87527a43c89a2d95bdc95897cf5fc20462194ad8
Claudia <claudeegarcia@icloud.com>
2021-03-30 23:02:50 -0500

    commit tree f7cac776cdce00442fde9094611ca63e2f2f121e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+
+//distance blocks equal 8. 
+//distance below 42
+//2cents per foot between 400-2000, the first 400ft free
+//25 dollars for a distance over 2000ft
+//no more than 2500
+
+//in instructions if block is over 42, get that number and subtract 42 out of it. if its under 42(n-42??), grab that number then its 42-n
+function distanceFromHqInBlocks(blocks){
+    if (blocks < 42) {
+        return 42 - blocks
+    } else if (blocks > 42) {
+        return blocks - 42
+    }
+}
+
+function distanceFromHqInFeet(){
+
+}
+
+function distanceTravelledInFeet(){
+
+}
